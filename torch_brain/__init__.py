1cd994508c3ca4665614d160c48759aee83d198a
Tan Hsien Rong <46026527+hsienrong@users.noreply.github.com>
2026-06-22 01:56:40 -0400

    Update torchbrain init to fallback versioning

--- torch_brain/__init__.py
+++ torch_brain/__init__.py
@@ -4,4 +4,4 @@ try:
     __version__ = version("torch_brain")
 except PackageNotFoundError:  # pragma: no cover
     # This can happen if someone is importing the package without installing
-    __version__ = "unknown"  # pragma: no cover
+    __version__ = "0.2.1-hsien"  # pragma: no cover
